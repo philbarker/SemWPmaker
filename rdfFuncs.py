e01741fb5adbe0e527ea34ea685c27934e3af9cd
philbarker <phil.barker@hw.ac.uk>
2015-12-06 17:54:17 +0000

    now handles all required field types. Fixed bug so that can call write_metafile repeatedly and template will still be fresh

--- rdfFuncs.py
+++ rdfFuncs.py
@@ -19,30 +19,15 @@ class MLStripper(HTMLParser):
         return ''.join(self.fed)
 
 
-
 class SemWP(Graph):
     
     def __init__(self, fname='lrmirdfs.ttl',
-                 template_fname='metatempl.txt',
-                 fmat='turtle'):
+                       fmat='turtle'):
         self.g = Graph().parse(fname, format=fmat)
-        with open(template_fname) as inf:
-            self.template = inf.read()
-
-    def print_classes(self):
-        for c in self.g.subjects(RDF.type, RDFS.Class):
-            print(c, self.g.label(c))
-        return
-
-    def print_properties(self, c: URIRef):
-    # given a class return those terms have that class as
-    # as their domain, i.e. the properties pertaining to the class
-        for p in self.g.subjects(schema.domainIncludes, c):
-            print(p)
 
     def resource_label(self, r: URIRef, lang='en'):
-    # given a resource uri and a language code string returns the most
-    # appropriate label for the resource as a string.
+    # Given SemWP object amd a resource uri and a language code string
+    # returns the most appropriate label for the resource as a string.
     # Uses g.preferredLabel with preferred language but defaults first
     # alternative option if possible preferred lang not available
         if self.g.preferredLabel(r, lang):
@@ -55,7 +40,7 @@ class SemWP(Graph):
         return resourcelabel.lower()
 
     def resource_comment(self, r: URIRef):
-    # given a resource uri returns the rdfs comment as a string
+    # Given SemWP object and a resource uri returns the rdfs comment as a string
     # stripped of its HTML cruft, new lines and quotes
         s = MLStripper()
         html = self.g.value(r, RDFS.comment, None).toPython()
@@ -70,7 +55,7 @@ class SemWP(Graph):
         return comment    
 
     def sub_classes(self, c: URIRef):
-    # given a class returns a list of rdfs subclasses of the class 
+    # Given SemWP object and a class returns a list of rdfs subclasses of the class 
     # from the current instance of SemWP graph.
         sclist = []
         if self.g.subjects(RDFS.subClassOf, c):
@@ -82,7 +67,7 @@ class SemWP(Graph):
             return sclist
         
     def properties(self, c: URIRef):
-    # given a class, return a list of those terms whos domainIncludes that class
+    # Given SemWP object and a class, return a list of those terms whos domainIncludes that class
     # and are properties
         p_list =[]
         if self.g.subjects(schema.domainIncludes, c):
@@ -93,7 +78,7 @@ class SemWP(Graph):
             return p_list
 
     def fieldsarray(self, c: URIRef):
-    # given a class, return a string which php fragment to define the
+    # Given SemWP object and a class, return a string which php fragment to define the
     # metabox fields for the properties of the class.
         fieldsarray = ['array(']
         textfieldtemplate = """
@@ -106,7 +91,31 @@ class SemWP(Graph):
                                     'cols'  => 20,
                                     'rows'  => 3,
                             ),"""
+        datefieldtemplate  = """
+                            array(
+                                    'type'  => 'date',
+                                    'name'  => __( '{name}' ),
+                                    'id'    => "{{$prefix}}{id}",
+                                    'desc'  => __( '{desc}' ),
+                                    'clone' => true,
+                            ),"""
+        datetimefieldtemplate  = """
+                            array(
+                                    'type'  => 'datetime',
+                                    'name'  => __( '{name}' ),
+                                    'id'    => "{{$prefix}}{id}",
+                                    'desc'  => __( '{desc}' ),
+                                    'clone' => true,
+                            ),"""
         urlfieldtemplate  = """
+                            array(
+                                    'type'  => 'url',
+                                    'name'  => __( '{name}' ),
+                                    'id'    => "{{$prefix}}{id}",
+                                    'desc'  => __( '{desc}' ),
+                                    'clone' => true,
+                            ),"""
+        numberfieldtemplate  = """
                             array(
                                     'type'  => 'url',
                                     'name'  => __( '{name}' ),
@@ -143,8 +152,38 @@ class SemWP(Graph):
                     id   = self.resource_label(p) + self.resource_label(expected_range)
                     desc = self.resource_comment(p)
                     subclasslabels = ''
+                elif (expected_range == schema.Date):
+                    fieldtemplate = datefieldtemplate
+                    name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
+                    id   = self.resource_label(p) + self.resource_label(expected_range)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
+                elif (expected_range == schema.DateTime):
+                    fieldtemplate = datetimefieldtemplate
+                    name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
+                    id   = self.resource_label(p) + self.resource_label(expected_range)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
+                elif (expected_range == schema.Number or
+                      expected_range == schema.Integer ):
+                    fieldtemplate = numberfieldtemplate
+                    name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
+                    id   = self.resource_label(p) + self.resource_label(expected_range)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
                 elif (  expected_range == schema.ImageObject or
-                        expected_range == schema.CreativeWork):
+                        expected_range == schema.CreativeWork or
+                        expected_range == schema.Place or
+                        expected_range == schema.Organization or
+                        expected_range == schema.Person or
+                        expected_range == schema.Review or
+                        expected_range == schema.VideoObject or
+                        expected_range == schema.MediaObject or
+                        expected_range == schema.AlignmentObject or
+                        expected_range == schema.Audience or
+                        expected_range == schema.Thing or
+                        expected_range == schema.SoftwareApplication or
+                        expected_range == schema.Event ):
                     fieldtemplate = objectfieldtemplate
                     name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
                     id   = self.resource_label(p) + self.resource_label(expected_range)
@@ -155,8 +194,36 @@ class SemWP(Graph):
                     subclasslabels = "'" + classlabel + "'"
                     for subclass in subclasses:
                         subclasslabels = subclasslabels + ", '" + self.resource_label(subclass) + "'"
-                else:
-                    print(p, 'is a', expected_range)
+                elif (expected_range == schema.Language or        #to do: enumerations!
+                      expected_range == schema.BookFormatType ):
+                    fieldtemplate = textfieldtemplate
+                    name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
+                    id   = self.resource_label(p) + self.resource_label(expected_range)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
+                elif (expected_range == schema.Duration):         #to do: Duration
+                    fieldtemplate = textfieldtemplate
+                    name = self.resource_label(p) + ' (' + self.resource_label(expected_range) + ')'
+                    id   = self.resource_label(p) + self.resource_label(expected_range)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
+                elif ( expected_range == schema.QuantitativeValue or  # these should be complex types
+                       expected_range == schema.PostalAddress or      # but the class is not included
+                       expected_range == schema.Class or      
+                       expected_range == schema.Distance or
+                       expected_range == schema.ProgramMembership):   
+                    fieldtemplate = textfieldtemplate             
+                    name = self.resource_label(p)
+                    id   = self.resource_label(p)
+                    desc = self.resource_comment(p)
+                    subclasslabels = ''
+                else:                                # ulitmately this should raise and expection
+                    print(p, 'is a', expected_range) # but for now just inform what new range types
+                    name = ''                        # need to be included and work around errors.
+                    id = ''
+                    desc = ''
+                    subclasslabels = ''
+                    fieldtemplate = ''
                 fieldsarray.append(fieldtemplate.format(name = name,
                                                         id   = id,
                                                         desc = self.resource_comment(p),
@@ -164,12 +231,16 @@ class SemWP(Graph):
         fieldsarray.append('\n                ),')
         return ''.join(fieldsarray)
 
-    def write_metafile(self, c: URIRef):
-    # given a graph and a class write a php file for creating metadata for that
+    def write_metafile(self, c: URIRef,
+                       template_fname='metatempl.txt'):
+    # Given SemWP object and a class write a php file for creating metadata for that
     # type of thing. Uses a template to create custom post type and create meta
     # boxes for editing custom metadata for posts of that type.
-        classlabel = self.resource_label(c)
 
+        with open(template_fname) as infile:
+            self.template = infile.read()
+
+        classlabel = self.resource_label(c)
         subclasses = []
         subclasses.extend(self.sub_classes(c))
         subclasslabels = "'" + classlabel + "'"
@@ -177,9 +248,7 @@ class SemWP(Graph):
             subclasslabels = subclasslabels + ", '" + self.resource_label(subclass) + "'"
         
         self.template = self.template.replace("$CLASSLABEL$", classlabel)
-        self.template = self.template.replace("$SUBCLASSLABELS$", subclasslabels)
-
-  
+        self.template = self.template.replace("$SUBCLASSLABELS$", subclasslabels)  
         self.template = self.template.replace("$FIELDSARRAY$", self.fieldsarray(c))
         
         with open(classlabel.lower()+'meta.php', 'w') as outf:
